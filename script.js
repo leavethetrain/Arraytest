854a7796d1f9c3138726ff54fd94f877c7c875f6
leavethetrain <musik201092@web.de>
2025-03-31 10:51:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,73 @@
+let array = [2, 3, 4, 8];
+
+function inputValue() {
+  const userInput = document.getElementById("input").valueAsNumber;
+  array.push(userInput);
+
+  document.getElementById("input").value = "";
+  showArray();
+}
+
+function deleteContent() {
+  const content = Number(document.getElementById("deleteContent").value);
+  const newListe = array.filter((element) => {
+    return element !== content;
+  });
+  array = newListe;
+
+  document.getElementById("deleteContent").value = "";
+  showArray();
+}
+
+/*function deleteContent() {
+  const zahl = document.getElementById("deleteContentt").value;
+  const inputValue = parseInt(zahl, 10);
+  console.log(inputValue);
+
+  const index = array.indexOf(inputValue);
+
+  if (index !== -1) {
+    array.splice(index, 1);
+    console.log("Das Element" + inputValue + " wurde gelöscht");
+  } else {
+    console.log("Das Element " + inputValue + " wurde nicht gefunden");
+  }
+
+  console.log(array);
+  document.getElementById("deleteContentt").value = "";
+}*/
+
+function deleteByPosition() {
+  const position = document.getElementById("deleteId").value;
+  if (position < array.length) {
+    array.splice(position, 1);
+  } else {
+    console.log("Die Position muss kleiner als " + array.length + " sein");
+  }
+  document.getElementById("deleteId").value = "";
+  showArray();
+}
+
+function multiplyArray() {
+  const multiplyArray = array.map((element) => {
+    return element * 2;
+  });
+
+  array = multiplyArray;
+  showArray();
+}
+
+function sumArray() {
+  const sumArray = array.reduce((previousValue, currentValue) => {
+    console.log(previousValue, currentValue);
+    return previousValue + currentValue;
+  });
+  console.log(sumArray);
+}
+
+function showArray() {
+  console.log(array);
+  array.forEach((element) => {
+    console.log(element);
+  });
+}
